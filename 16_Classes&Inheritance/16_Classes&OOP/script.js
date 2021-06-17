ad795e9b9e46a68cccaddaefc3bfab2b3fd19a9a
Peter Ilyash <p.ilyash.j@gmail.com>
2021-06-17 16:20:36 +0300

    Update

--- 16_Classes&Inheritance/16_Classes&OOP/script.js
+++ 16_Classes&Inheritance/16_Classes&OOP/script.js
@@ -1,29 +1,29 @@
 class Employee {
-    constructor(f, r, d){
+    constructor(f, r, d) {
         this.fullName = f;
         this.rate = r;
         this.days = d;
-        employees[employeesList].append = this;
+        Employee.employeesList.push(this);
+        // console.log(Employee.employeesList);
     }
     static employeesList = [];
-    getSalary(){
-        let  salary = 0;
-        return salary = this.rate * this.days;
+    getSalary() {
+        return this.rate * this.days;
     }
 
-    static getGeneralSum(){
-       let generalSum = 0;
-       for(let salary of employees[employeesList]){
-           generalSum += salary;
-       }
-       return generalSum;
+    static getGeneralSum() {
+        let generalSum = 0;
+        for (let salary of Employee.employeesList.getSalary()) {
+            generalSum += salary;
+        }
+        return generalSum;
     }
 };
 
-    const jack = new Employee('Jack Shepard', 500, 22);
-    jack.salary = jack.getSalary();
-    const tom =  new Employee('Tom Atkins', 100, 25);
-    tom.salary = tom.getSalary();
+const jack = new Employee('Jack Shepard', 500, 22);
+jack.salary = jack.getSalary();
+const tom = new Employee('Tom Atkins', 100, 25);
+tom.salary = tom.getSalary();
 
 console.log(jack.salary); // 11000
 console.log(tom.salary); // 2500 
